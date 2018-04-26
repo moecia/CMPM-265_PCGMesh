62e0c3c83632f5706b3bbed43616e77578860fba
Bingnan Li, Nathan <leebingnam@hotmail.com>
2018-04-25 18:44:22 -0700

    Complete version

---
+++ Assets/02_Scripts/LevelLoader.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour {
+
+    public void LoadLevel(int index)
+    {
+        Application.LoadLevel(index);
+    }
+}
